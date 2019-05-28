83add85238929485cf865a6b2cca42a171b5da5a
Wei Weng <weweng@microsoft.com>
2019-05-28 08:13:19 -0700

    Remove Its.Log.Monitoring, Its.Log.Monitoring.UI, and Its.Log.Monitoring.UnitTests since jonsequitur said that they are not needed anymore.

--- Its.Log.Monitoring.UI/test/applications.js
+++
@@ -1,8 +0,0 @@
-(function(mockTests) {
-  if (typeof module === 'object') {
-    module.exports = mockTests
-  } else {
-    window.fakeServer.respondWith('GET', '/tests/applications',
-      [200, { 'Content-Type': 'application/json' }, JSON.stringify(mockTests)]);
-  }
-})(['ordering', 'publishing'])
--- Its.Log.Monitoring.UI/test/environments.js
+++
@@ -1,8 +0,0 @@
-(function(mockTests) {
-  if (typeof module === 'object') {
-    module.exports = mockTests
-  } else {
-    window.fakeServer.respondWith('GET', '/tests/environments',
-      [200, { 'Content-Type': 'application/json' }, JSON.stringify(mockTests)]);
-  }
-})(['done', 'production'])
